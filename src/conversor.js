e639e847c7bd5ad8fc8ab2ff11af3959bcd76ffd
nathan95oks <nathanaelalba75@gmail.com>
2025-02-20 19:05:05 -0400

    convertir el numero 1 y 2 a romano

--- src/conversor.js
+++ src/conversor.js
@@ -2,6 +2,9 @@ function convertir(a){
     if(a===1){
     return "I";
     };
+    if(a===2){
+        return "II";
+    };
 };
 
 export default convertir;
--- src/conversor.test.js
+++ src/conversor.test.js
@@ -4,4 +4,7 @@ describe("Deberia convertir los numeros a romanos",()=>{
    it("Deberia convertir un numero 1",()=>{
      expect(convertir(1)).toEqual("I");
    });
+   it("Deberia convertir un numero 1",()=>{
+    expect(convertir(2)).toEqual("II");
+  });
 });
